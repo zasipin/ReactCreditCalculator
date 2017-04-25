4f7b3d381c1ec0e69873d55e33015d20211ad088
10-1-27-221-WV\ZasypinNV <ZasypinNV@yanos.slavneft.ru>
2017-04-25 13:14:03 +0300

    Added styles to nav menu

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,13 +4,19 @@ import {Link, IndexLink} from 'react-router';
 class Nav extends React.Component{
   render () {
     return (
-    	<div>
-      		<h2>Nav Component</h2>
-        	<IndexLink to="/" activeClassName="active">Потребительский кредит</IndexLink>
-        	<Link to="/mortage" activeClassName="active">Ипотека</Link>
-        	<Link to="/car" activeClassName="active">Автокредит</Link>
-        	<Link to="/about" activeClassName="active">О проекте</Link>
+			<div className="row top-menu">
+				
+    		<div className="small-centered large-10 small-12 columns">
+      		<ul className="menu menu-centered">
+        		<li><IndexLink to="/" activeClassName="active">Потребительский кредит</IndexLink></li>
+        		<li><Link to="/mortage" activeClassName="active">Ипотека</Link></li>
+        		<li><Link to="/car" activeClassName="active">Автокредит</Link></li>
+        		<li><Link to="/about" activeClassName="active">О проекте</Link></li>
+					</ul>
       	</div>
+				
+				
+			</div>
     );
   }
 };
