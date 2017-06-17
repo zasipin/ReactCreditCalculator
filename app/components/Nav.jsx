02d0212597918bc368ce3d04749796da4da538eb
zasipin <zasipin.it@gmail.com>
2017-06-17 19:40:20 +0300

    Top menu styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,10 +8,10 @@ class Nav extends React.Component{
 				
     		<div className="small-centered large-10 small-12 columns menu-centered">
       		<ul className="menu">
-        		<li><IndexLink to="/" activeClassName="active">Потребительский кредит</IndexLink></li>
-        		<li><Link to="/mortage" activeClassName="active">Ипотека</Link></li>
-        		<li><Link to="/car" activeClassName="active">Автокредит</Link></li>
-        		<li><Link to="/about" activeClassName="active">О проекте</Link></li>
+        		<li><IndexLink to="/" activeClassName="active">ПОТРЕБИТЕЛЬСКИЙ</IndexLink></li>
+        		<li><Link to="/mortage" activeClassName="active">ИПОТЕКА</Link></li>
+        		<li><Link to="/car" activeClassName="active">АВТОКРЕДИТ</Link></li>
+        		<li><Link to="/about" activeClassName="active">О ПРОЕКТЕ</Link></li>
 					</ul>
       	</div>			
 				
