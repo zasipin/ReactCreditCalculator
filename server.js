3c8df9e12e1f522ed1a441fdc01c10d457ff316c
10-1-27-221-WV\ZasypinNV <ZasypinNV@yanos.slavneft.ru>
2017-08-29 10:48:21 +0300

    defined app addresses in server.js

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
+app.use((req, res, next) => {
+  const appUrls = ['/consumer', '/mortage', '/car']
+  if(appUrls.findIndex((val) => { return req.url === val; }) >= 0)
+    res.redirect('http://' + req.hostname + ':' + PORT + '/');
+  else
     next();
-  }
+});
+
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
 });
 
 app.use(express.static('public'));
