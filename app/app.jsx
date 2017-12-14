67749b809e5a27c400582751a085ce917cdab4ba
zasipin <ZasypinNV@yanos.slavneft.ru>
2017-12-14 11:00:38 +0300

    add utils to app.js

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-
+import { getClientLanguage } from './utils/common_utils';
 // import jquery from 'jquery';
 // import foundation from 'foundation-sites';
 
