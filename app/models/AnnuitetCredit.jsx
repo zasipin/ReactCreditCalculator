afec800b954255f33befc7d28f08e805f1ec6d33
zasipin <ZasypinNV@yanos.slavneft.ru>
2017-12-19 14:32:21 +0300

    recalculate acredit payments - NaN bug

--- app/models/AnnuitetCredit.jsx
+++ app/models/AnnuitetCredit.jsx
@@ -17,13 +17,13 @@ export class AnnuitetCredit{
         return this.recalculate(this.state.sum, this.state.percents);
     }
 
-    recalculate(sum, percents) {
+    recalculate(sum, percents = 0) {
             var monthlyPercent = percents / 12 / AnnuitetCredit.ToPercents;
             var power = Math.pow((1 + monthlyPercent), this.state.months);
             var state = this.state;
             state.sum = sum;
             state.percents = percents;
-            state.annuitetCoefficient = monthlyPercent * power / (power - 1);
+            state.annuitetCoefficient = power === 1 ? 1 / state.months : monthlyPercent * power / (power - 1);
             state.monthlyPay = sum * state.annuitetCoefficient;
             state.totalPay = state.monthlyPay * state.months;
             state.overpay = state.totalPay - sum;
