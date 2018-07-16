7b705bfc04e66e3601747a5d38c92fe7322ead33
zasipin <ZasypinNV@yanos.slavneft.ru>
2018-07-16 10:43:33 +0300

    fix: Common utils getLanguage ret 2 chars

--- app/utils/common_utils.js
+++ app/utils/common_utils.js
@@ -1,6 +1,6 @@
 export function getClientLanguage(){
   if(navigator.browserLanguage)
-    return navigator.browserLanguage;
+    return navigator.browserLanguage.slice(0,2);
   if(navigator.language)  
-    return navigator.language;
+    return navigator.language.slice(0,2);
 }
